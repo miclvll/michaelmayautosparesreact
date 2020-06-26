44e5b5a2656caf4a4b9bfbafdd91f15a2986989c
mike <jeffreymaeps@gmail.com>
2020-06-26 17:45:41 +0100

    amend package.json

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+//import logo from "./images/logo.png";
+import "./App.css";
 import Home from "./pages/Home";
 import Parts from "./pages/Parts";
 import SinglePart from "./pages/SinglePart";
 import Error from "./pages/Error";
-import {Route,Switch} from 'react-router-dom';
+import { Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Cart from "./components/Cart";
 import "bootstrap/dist/css/bootstrap.min.css";
- 
 
 function App() {
   return (
     <>
-    <Navbar/>
-    <Switch>
-    <Route exact path ="/" component = {Home}/>
-    <Route exact path ="/Parts" component = {Parts}/>
-    <Route exact path="/Parts/:slug" component = {SinglePart}/>
-    <Route component ={Error}/>
-    </Switch>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/Parts" component={Parts} />
+        <Route exact path="/Parts/:slug" component={SinglePart} />
+        <Route component={Error} />
+      </Switch>
     </>
-      )}
+  );
+}
 
 export default App;
